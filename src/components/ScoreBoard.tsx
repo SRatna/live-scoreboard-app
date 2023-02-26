5c55513b4b26c455890a54bd3fc7c150c64ea40a
sratna <adhikariratna52@gmail.com>
2023-02-26 12:56:13 +0100

    add score board list with some dummy data

--- src/components/ScoreBoard.tsx
+++ src/components/ScoreBoard.tsx
@@ -1,14 +1,31 @@
 import { Typography, Button } from 'antd';
+import { Score } from '../models/Score';
+import ScoreBoardList from './ScoreBoardList';
 
 const { Title } = Typography;
 
 const ScoreBoard = () => {
+  const scores: Score[] = [
+    {
+      homeTeam: "Nepal",
+      homeTeamScore: 2,
+      awayTeam: "India",
+      awayTeamScore: 4
+    },
+    {
+      homeTeam: "Mexico",
+      homeTeamScore: 1,
+      awayTeam: "Spain",
+      awayTeamScore: 4
+    }
+  ]
   return (
     <>
       <div style={{ display: "flex", justifyContent: "space-between" }}>
         <Title style={{ marginTop: 0 }} level={4}>Live Score Board</Title>
         <Button type="primary">Start New Game</Button>
       </div>
+      <ScoreBoardList scores={scores} />
     </>
   )
 }
---
+++ src/components/ScoreBoardList.tsx
@@ -0,0 +1,23 @@
+import { List } from 'antd';
+import { Score } from '../models/Score';
+
+interface PropType {
+  scores: Score[]
+}
+
+const ScoreBoardList = ({ scores }: PropType) => {
+  return (
+    <List
+      itemLayout="horizontal"
+      dataSource={scores}
+      renderItem={(item) => (
+        <List.Item>
+          <List.Item.Meta
+            title={`${item.homeTeam} ${item.homeTeamScore} - ${item.awayTeam} ${item.awayTeamScore}`}
+          />
+        </List.Item>
+      )}
+    />
+  );
+}
+export default ScoreBoardList;
