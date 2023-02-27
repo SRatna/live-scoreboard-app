51076aaa30b692e7df973577fcd4c16ff658b65c
sratna <adhikariratna52@gmail.com>
2023-02-27 10:26:59 +0100

    implement update score method and its tests

--- src/helpers/index.test.ts
+++ src/helpers/index.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from '@jest/globals';
 import { Game } from '../models/Game';
-import { startNewGame, addNewGameToScoreboard } from './index';
+import { startNewGame, addNewGameToScoreboard, updateScore } from './index';
 
 describe('helper function: start new game', () => {
   test('throws: empty team names', () => {
@@ -35,5 +35,48 @@ describe('helper function: add new game to scoreboard', () => {
   test('scoreboard should have new Game at first', () => {
     const scoreboard = addNewGameToScoreboard(currentGames, newGame);
     expect(scoreboard[0]).toBe(newGame);
+  });
+});
+
+describe('helper function: update score', () => {
+  const games: Game[] = [
+    {
+      id: '123',
+      homeTeam: 'abc',
+      homeTeamScore: 0,
+      awayTeam: 'xyz',
+      awayTeamScore: 1,
+      startedAt: Date.now()
+    }
+  ]
+
+  test('throws on empty games', () => {
+    expect(() => { updateScore([], '', 0, 0) }).toThrow('Empty scoreboard!');
+  });
+
+  test('throws on empty game id', () => {
+    expect(() => { updateScore(games, '', 0, 0) }).toThrow('Game not found!');
+  });
+
+  test('throws on invalid game id', () => {
+    expect(() => { updateScore(games, '321', 0, 0) }).toThrow('Game not found!');
+  });
+
+  test('throws on negative home team score', () => {
+    expect(() => { updateScore(games, '123', -2, 0) }).toThrow('Score cannot be less than zero!');
+  });
+
+  test('throws on negative away team score', () => {
+    expect(() => { updateScore(games, '123', 2, -2) }).toThrow('Score cannot be less than zero!');
+  });
+
+  test('throws on negative scores', () => {
+    expect(() => { updateScore(games, '123', -2, -2) }).toThrow('Score cannot be less than zero!');
+  });
+
+  test('returns new scoreboard with updated score', () => {
+    const scoreboard: Game[] = updateScore(games, '123', 1, 2);
+    expect(scoreboard[0].homeTeamScore).toBe(1);
+    expect(scoreboard[0].awayTeamScore).toBe(2);
   });
 });
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -27,3 +27,22 @@ export const addNewGameToScoreboard = (currentGames: Game[], newGame: Game) => {
     ...currentGames
   ];
 }
+
+export const updateScore = (
+  currentGames: Game[], gameId: string, homeTeamScore: number, awayTeamScore: number
+) => {
+  if (!currentGames.length) throw new Error('Empty scoreboard!');
+  const game: Game | undefined = currentGames.find(({ id }) => id === gameId);
+  if (!game) throw new Error('Game not found!');
+  if (homeTeamScore < 0 || awayTeamScore < 0) throw new Error('Score cannot be less than zero!');
+  return currentGames.map((game) => {
+    if (game.id === gameId) {
+      return {
+        ...game,
+        homeTeamScore,
+        awayTeamScore
+      }
+    }
+    return game;
+  });
+}
